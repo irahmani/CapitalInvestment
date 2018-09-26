d16c87b5d3726f68424c078211e6282133d64a45
Ibrahim <ibrahim@xala.no>
2018-09-26 21:41:45 +0200

    Add shares model
    Add new shares model and migration.

---
+++ database/migrations/2018_09_26_193923_create_shares_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('person_id');
+            $table->integer('company_id');
+            $table->integer('share');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shares');
+    }
+}
